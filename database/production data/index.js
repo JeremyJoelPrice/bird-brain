eb17e95579a6b0833d8b7d35ae9f6ba8d491666b
Jeremy Price <jeremy@jeremys-imac.home>
2022-03-01 10:21:12 +0000

    Update production data

--- database/production data/index.js
+++ database/production data/index.js
@@ -1,3 +1,50 @@
-exports.users = [];
+exports.users = [
+	{
+		username: "Paki Wong",
+		email: "vel.mauris.integer@aol.net",
+		password: "NLB65TPC5LE"
+	}
+];
 exports.fact_cards = require("./fact_cards");
-exports.users_fact_cards = [];
+exports.users_fact_cards = [
+	{
+		user_id: 1,
+		card_id: 1
+	},
+	{
+		user_id: 1,
+		card_id: 2
+	},
+	{
+		user_id: 1,
+		card_id: 50
+	},
+	{
+		user_id: 1,
+		card_id: 51
+	},
+	{
+		user_id: 1,
+		card_id: 52
+	},
+	{
+		user_id: 1,
+		card_id: 80
+	},
+	{
+		user_id: 1,
+		card_id: 81
+	},
+	{
+		user_id: 1,
+		card_id: 82
+	},
+	{
+		user_id: 1,
+		card_id: 83
+	},
+	{
+		user_id: 1,
+		card_id: 90
+	}
+];
